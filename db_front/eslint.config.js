1ac16fd6ef852e0fa97a15e05d5964ca348e9cc7
Andrii <andriikytrysh@gmail.com>
2026-01-08 13:43:04 +0100

    #master created and configured basic react+vite core of project

---
+++ db_front/eslint.config.js
@@ -0,0 +1,49 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import parser from '@typescript-eslint/parser';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist', 'node_modules']),
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser, 
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      '@typescript-eslint': tseslint.plugin,
+    },
+    settings: {
+      react: {
+        version: '18.0',
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      ...prettier.rules,
+    },
+  },
+]);
---
+++ db_front/src/App.tsx
@@ -0,0 +1,10 @@
+import type React from 'react'
+import './App.css'
+
+const App: React.FC = () => {
+  return (
+    <>Hello World!</>
+  )
+}
+
+export default App
