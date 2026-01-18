dcd3434eddebb5d6cb546dbce3c3c14850d56e6c
Andrii <andriikytrysh@gmail.com>
2026-01-18 12:38:04 +0100

    #master finaly working docker compose up with working db

--- db_backend/utils/connection.py
+++ db_backend/utils/connection.py
@@ -1,10 +1,11 @@
+import os
 import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        dbname="ks_bd",
-        user="postgres",
-        password="root",
-        host="localhost",
-        port="5432"
+        host=os.getenv("DB_HOST", "localhost"),
+        port=os.getenv("DB_PORT", "5432"),
+        dbname=os.getenv("DB_NAME", "ks_bd"),
+        user=os.getenv("DB_USER", "postgres"),
+        password=os.getenv("DB_PASS", "root")
     )
